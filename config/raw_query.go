909f47cf0d6a95fd854c3f0425013651e2e28303
yaminmuhammad <1910631170108@student.unsika.ac.id>
2023-12-06 21:22:04 +0700

    :rocket: delete comment

--- config/raw_query.go
+++ config/raw_query.go
@@ -6,6 +6,4 @@ const (
 	SelectTaskByAuthorID = `SELECT id, title, content, author_id, created_at, updated_at FROM tasks WHERE id = $1 ORDER BY created_at DESC`
 
 	SelectAuthorByID = `SELECT id, name, email, created_at, updated_at FROM authors WHERE id = $1`
-	// SelectAuthorList = `SELECT ID, name, email, created_at, updated_at FROM authors ORDER BY created_at desc`
-	// InsertAuthor     = `INSERT INTO authors (name, email, updated_at) VALUES($1, $2, $3) RETURNING id, created_at`
 )
